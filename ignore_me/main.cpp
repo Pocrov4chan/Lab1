85e4715d6de5d6400cf3dfa20550b0420a5fea63
Pocrov4chan <d_anikieiev@kbtu.kz>
2024-05-08 15:36:19 +0500

    I completely forgot

--- ignore_me/main.cpp
+++ ignore_me/main.cpp
@@ -1,171 +1,19 @@
-#include <cstdio>
-
-#include <cstring>
-
+#include <iostream>
 #include <vector>
-
-#include <queue>
-
-#define MAX 2010
-
-#define INF 0x3F3F3F3F
 
 using namespace std;
 
- 
-
-int i, j, v, d, to, cost, n, m, s, f, b, e, w;
-
-int used[MAX], dist[MAX], parent[MAX];
-
- 
-
-struct edge
-
-{
-
-  int node, dist;
-
-  edge(int node, int dist) : node(node), dist(dist) {}
-
-};
-
- 
-
-bool operator< (edge a, edge b)
-
-{
-
-  return a.dist > b.dist;
-
-}
-
- 
-
-vector<vector<edge> > g;
-
-priority_queue<edge> pq;
-
- 
-
-void Relax(int v, int to, int cost)
-
-{
-
- if (dist[to] > dist[v] + cost)
-
- {
-
-   dist[to] = dist[v] + cost;
-
-   pq.push(edge(to,dist[to]));
-
-   parent[to] = v;
-
- }
-
-}
-
- 
-
-void path(int v)
-
-{
-
-  if (v == -1) return;
-
-  path(parent[v]);
-
-  if (parent[v] != -1) printf(" ");
-
-  printf("%d",v);
-
-}
-
- 
-
-int main(void)
-
-{
-    freopen("distance.in", "r", stdin);
-    freopen("distance.out", "w", stdout);
-
-  scanf("%d %d",&n,&m);
-
-  scanf("%d %d",&s,&f);
-
-  g.resize(n+1);
-
-  for(i = 0; i < m; i++)
-
-  {
-
-    scanf("%d %d %d",&b,&e,&w);
-
-    g[b].push_back(edge(e,w));
-
-    g[e].push_back(edge(b,w));
-
+int count(vector <pair <int, int>> array, int endpoint, int stage, int sum){
+  if (stage == endpoint){
+    return sum % 2;
   }
 
- 
+  int branch_1 = count(array, endpoint, stage + 1, sum + array[stage].first);
+  int branch_2 = count(array, endpoint, stage + 1, sum + array[stage].second);
 
-  memset(dist,0x3F,sizeof(dist));
+  return branch_1 + branch_2;
+}
 
-  dist[s] = 0;
-
-  memset(parent,-1,sizeof(parent));
-
-  memset(used,0,sizeof(used));
-
- 
-
-  pq.push(edge(s,0));
-
-  while(!pq.empty())
-
-  {
-
-    edge e = pq.top(); pq.pop();
-
-    v = e.node;
-
-    if (e.dist > dist[v]) continue;
-
-    for(j = 0; j < g[v].size(); j++)
-
-    {
-
-      to = g[v][j].node;
-
-      cost = g[v][j].dist;
-
-      if (!used[to]) Relax(v,to,cost);
-
-    }
-
-    used[v] = 1;
-
-  }
-
- 
-
-  if (dist[f] == INF)
-
-    printf("-1\n");
-
-  else
-
-  {
-
-    printf("%d\n",dist[f]);
-
-    path(f); printf("\n");
-
-  }
-
- 
-
-  return 0;
-
+int main(){
+  
 }
